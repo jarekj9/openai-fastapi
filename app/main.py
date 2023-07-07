49fd070a87cde2db16ae1158dd200035e977c25c
Jaroslaw <jaroslaw.jankun@gmail.com>
2023-07-07 13:54:05 +0200

    Port should be 8000 by default for azure app service

--- app/main.py
+++ app/main.py
@@ -46,4 +46,4 @@ async def question_post(request: Request):
     return {"response": "bad input"}
 
 if __name__ == '__main__':
-    uvicorn.run('main:app', host='0.0.0.0', port=8003)
+    uvicorn.run('main:app', host='0.0.0.0', port=8000)
